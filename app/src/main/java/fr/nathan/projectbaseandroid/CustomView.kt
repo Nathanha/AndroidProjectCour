3d62879aa93a6f459542d63ad6c1fcb35f5c4bdd
Nathan <haro.nathan@gmail.com>
2018-11-06 09:17:15 +0100

    Ajout de l'étoile sur écran anko

--- app/src/main/java/fr/nathan/projectbaseandroid/CustomView.kt
+++ app/src/main/java/fr/nathan/projectbaseandroid/CustomView.kt
@@ -4,17 +4,23 @@ import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
+import android.graphics.Path
 import android.util.AttributeSet
 import android.view.View
 
 class CustomView : View {
 
     companion object {val DELTA = 8}
-    private var mPaint = Paint()
+
     lateinit var mCircle: MagicCircle
+
+    private var mPaint = Paint()
+    private val mHeartPath = Path()
+
     override fun onLayout(changed: Boolean, left: Int, top: Int, right: Int, bottom: Int) {
         super.onLayout(changed, left, top, right, bottom)
         mCircle = MagicCircle(width.toFloat(), height.toFloat())
+        createGraphicalObject()
         mCircle.delta = DELTA
 
     }
@@ -22,15 +28,36 @@ class CustomView : View {
     constructor(context: Context) : this(context, null)
     constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs) {init()}
 
+    private fun createGraphicalObject() {
+        mHeartPath.set(createHeart())
+    }
+
+    private fun createHeart(): Path {
+        val path = Path()
+        path.moveTo(50F, 30F)
+        path.lineTo(45F, 45F)
+        path.lineTo(30F, 46F)
+        path.lineTo(42F, 55F)
+        path.lineTo(35F, 67F)
+        path.lineTo(50F, 60F)
+        path.lineTo(65F, 67F)
+        path.lineTo(57F, 55F)
+        path.lineTo(70F, 46F)
+        path.lineTo(55F, 45F)
+        return path
+    }
+
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
         mCircle.move()
+        canvas?.drawPath(mHeartPath, mPaint)
         canvas?.drawCircle(mCircle.cx, mCircle.cy, mCircle.rad, mPaint)
         invalidate()
     }
 
     private fun init()
     {
+        mPaint.style = Paint.Style.FILL
         mPaint.color = Color.BLUE
     }
 }
--- app/src/main/java/fr/nathan/projectbaseandroid/InscriptionFragment.kt
+++ app/src/main/java/fr/nathan/projectbaseandroid/InscriptionFragment.kt
@@ -5,6 +5,10 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.graphics.Color.parseColor
+import android.widget.NumberPicker
+
+
 
 class InscriptionFragment : Fragment() {
 
@@ -12,8 +16,18 @@ class InscriptionFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
+        val root = inflater.inflate(R.layout.fragment_inscription, container, false)
+
+        val np = root.findViewById(R.id.npAge) as NumberPicker
+
+        np.minValue = 0
+        np.maxValue = 10
+        np.wrapSelectorWheel = false
+
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_inscription, container, false)
+        return root
     }
 
 
